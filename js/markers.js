22e8cc31d4c490d04540a77ca11643ab41dcc15e
MightyKnightsOfTheSouth <phiman645@phimen.us>
2026-08-03 18:13:34 -0400

    Add markers for collegiate data on the map

---
+++ js/markers.js
@@ -0,0 +1,81 @@
+const markerMap = new Map();
+
+
+collegiateData.forEach(college => {
+
+    let markerImg;
+    let pinBgColor;
+    let pinBorderColor;
+
+
+    // Expansion Candidate
+    if (college.exists === false) {
+
+        markerImg = "images/pspexpansion.PNG";
+        pinBgColor = "#4b5563";
+        pinBorderColor = "#374151";
+
+
+    // Existing Active Chapter
+    } else if (college.active === true) {
+
+        markerImg = "images/pspexists.png";
+        pinBgColor = "#15803d";
+        pinBorderColor = "#166534";
+
+
+    // Existing Inactive Chapter
+    } else {
+
+        markerImg = "images/pspexists.png";
+        pinBgColor = "#780606";
+        pinBorderColor = "#500404";
+
+    }
+
+
+    const customIcon = L.divIcon({
+
+        html: `
+            <div class="custom-pin-container"
+                 style="--pin-bg:${pinBgColor};
+                        --pin-border:${pinBorderColor};">
+
+                <div class="custom-pin-head"
+                     style="
+                     background-image:url('${markerImg}');
+                     background-color:${pinBgColor};
+                     ">
+                </div>
+
+            </div>
+        `,
+
+        className: "",
+        iconSize: [42,50],
+        iconAnchor: [21,50]
+
+    });
+
+
+
+    const marker = L.marker(
+        college.coordinates,
+        {
+            icon: customIcon
+        }
+    );
+
+
+    markerMap.set(college.id,{
+        marker: marker,
+        data: college
+    });
+
+
+
+    marker.addTo(map);
+
+
+
+});
