1060891b9d9c5dd61f0cb64dda45c9a07747b22a
MightyKnightsOfTheSouth <phiman645@phimen.us>
2026-08-03 18:48:52 -0400

    Refactor loadMarkers function for clarity and structure

--- js/markers.js
+++ js/markers.js
@@ -7,173 +7,144 @@ const markerMap = new Map();
 
 function loadMarkers() {
 
-collegiateData.forEach(college => {
 
-    let markerImg;
-    let pinBgColor;
-    let pinBorderColor;
+    collegiateData.forEach(college => {
 
 
-    // Expansion Candidate
-    if (college.exists === false) {
-
-        markerImg = "images/pspexpansion.PNG";
-        pinBgColor = "#4b5563";
-        pinBorderColor = "#374151";
+        let markerImg;
+        let pinBgColor;
+        let pinBorderColor;
 
 
-    // Existing Active Chapter
-    } else if (college.active === true) {
+        // Expansion Candidate
+        if (college.exists === false) {
 
-        markerImg = "images/pspexists.png";
-        pinBgColor = "#15803d";
-        pinBorderColor = "#166534";
-
-
-    // Existing Inactive Chapter
-    } else {
-
-        markerImg = "images/pspexists.png";
-        pinBgColor = "#780606";
-        pinBorderColor = "#500404";
-
-    }
+            markerImg = "images/pspexpansion.PNG";
+            pinBgColor = "#4b5563";
+            pinBorderColor = "#374151";
 
 
-    const customIcon = L.divIcon({
+        // Existing Active Chapter
+        } else if (college.active === true) {
 
-        html: `
-            <div class="custom-pin-container"
-                 style="--pin-bg:${pinBgColor};
-                        --pin-border:${pinBorderColor};">
+            markerImg = "images/pspexists.png";
+            pinBgColor = "#15803d";
+            pinBorderColor = "#166534";
 
-                <div class="custom-pin-head"
-                     style="
-                     background-image:url('${markerImg}');
-                     background-color:${pinBgColor};
-                     ">
+
+        // Existing Inactive Chapter
+        } else {
+
+            markerImg = "images/pspexists.png";
+            pinBgColor = "#780606";
+            pinBorderColor = "#500404";
+
+        }
+
+
+
+        const customIcon = L.divIcon({
+
+            html: `
+                <div class="custom-pin-container"
+                     style="--pin-bg:${pinBgColor};
+                            --pin-border:${pinBorderColor};">
+
+                    <div class="custom-pin-head"
+                         style="
+                         background-image:url('${markerImg}');
+                         background-color:${pinBgColor};
+                         ">
+                    </div>
+
                 </div>
+            `,
 
-            </div>
-        `,
+            className: "",
+            iconSize: [42,50],
+            iconAnchor: [21,50]
 
-        className: "",
-        iconSize: [42,50],
-        iconAnchor: [21,50]
+        });
+
+
+
+        const marker = L.marker(
+            college.coordinates,
+            {
+                icon: customIcon
+            }
+        );
+
+
+
+        markerMap.set(college.id,{
+            marker: marker,
+            data: college
+        });
+
+
+
+        marker.bindPopup(
+            getDetailProfileHtml(college),
+            {
+                direction: "auto",
+                offset: [0,-20],
+                closeButton: false,
+                autoPan: false
+            }
+        );
+
+
+
+        marker.on("click", () => {
+
+            focusLocation(college);
+
+        });
+
+
+
+        marker.on("mouseover", (e)=>{
+
+            hoverCard.innerHTML = getSidebarCardHtml(college);
+
+            hoverCard.style.display = "block";
+
+            updateHoverPosition(e.originalEvent);
+
+        });
+
+
+
+        marker.on("mousemove",(e)=>{
+
+            updateHoverPosition(e.originalEvent);
+
+        });
+
+
+
+        marker.on("mouseout",()=>{
+
+            hoverCard.style.display = "none";
+
+        });
+
+
+
+        marker.on("popupopen",()=>{
+
+            hoverCard.style.display = "none";
+
+        });
+
+
+
+        marker.addTo(map);
+
 
     });
 
-function loadMarkers() {
+}
 
-collegiateData.forEach(college => {
-
-    const marker = L.marker(
-        college.coordinates,
-        {
-            icon: customIcon
-        }
-    );
-
-
-    markerMap.set(college.id,{
-        marker: marker,
-        data: college
-    });
-
-
-marker.bindPopup(getDetailProfileHtml(college), {
-    direction: 'auto',
-    offset: [0,-20],
-    closeButton: false,
-    autoPan: false
-});
-
-
-marker.on('mouseover', (e)=>{
-
-    hoverCard.innerHTML = getSidebarCardHtml(college);
-
-    hoverCard.style.display = "block";
-
-    updateHoverPosition(e.originalEvent);
-
-});
-
-
-marker.on('mouseout',()=>{
-
-    hoverCard.style.display = "none";
-
-});
-
-
-marker.addTo(map);
-
-
-
-    // =============================
-    // Popup
-    // =============================
-
-    marker.bindPopup(
-        getDetailProfileHtml(college),
-        {
-            direction: "auto",
-            offset: [0,-20],
-            closeButton: false,
-            autoPan: false
-        }
-    );
-
-
-
-    // =============================
-    // Click Marker
-    // =============================
-
-    marker.on("click", () => {
-        focusLocation(college);
-    });
-
-
-
-    // =============================
-    // Hover Preview
-    // =============================
-
-    marker.on("mouseover", (e) => {
-
-        hoverCard.innerHTML = getSidebarCardHtml(college);
-
-        hoverCard.style.display = "block";
-
-        updateHoverPosition(e.originalEvent);
-
-    });
-
-
-
-    marker.on("mousemove", (e) => {
-
-        updateHoverPosition(e.originalEvent);
-
-    });
-
-
-
-    marker.on("mouseout", () => {
-
-        hoverCard.style.display = "none";
-
-    });
-
-
-
-    marker.on("popupopen", () => {
-
-        hoverCard.style.display = "none";
-
-    });
-
-
-});
+loadMarkers();
