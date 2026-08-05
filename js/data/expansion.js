c120a4fc62ff6e9d759c31254ed9eba961889ef1
MightyKnightsOfTheSouth <phiman645@phimen.us>
2026-08-04 21:27:30 -0400

    Add expansion data structure for opportunities

---
+++ js/data/expansion.js
@@ -0,0 +1,13 @@
+const expansion = [
+
+    {
+        id: "example",
+        name: "Example University",
+        subtitle: "Expansion Opportunity",
+        locationName: "City, State",
+        coordinates: [35.0000,-80.0000],
+        exists:false,
+        active:false
+    }
+
+];
